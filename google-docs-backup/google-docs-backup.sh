ce6bff1140dd8738eb2967f54699b0be3f178894
Petri Heikkinen <84968030+sloul1@users.noreply.github.com>
2024-07-30 02:11:02 +0300

    Update google-docs-backup.sh
    updated instructions, some structural changes

--- google-docs-backup/google-docs-backup.sh
+++ google-docs-backup/google-docs-backup.sh
@@ -5,25 +5,30 @@
 # giving worksheet edit permissions were considered for all team members.
 # Hmm...what could go wrong with this? =)
 
-# Script uses .env file with variables as mentioned below.
+# Script uses .env file with variables mentioned below.
+# .env file should exist in same location with this script.
+# Script assumes that downloaded files are kept in same directory
+# with this script and no subdirectories are used.
+
 # Edit .env file variables according to your needs
 
 # SPREADSHEET_LINK="https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID_HERE/export?format=xlsx"
-# BACKUP_FILENAME="Gotham-City-FC-team-uniforms"
+# BACKUP_FILENAME="type string of your choice here for identifying your backup files"
 # BACKUP_DIR="/path/to/your/backup/directory"
 # LOG_FILE="${BACKUP_DIR}/backup_log.txt"
 
-# Script assumes that '.env' file exist in same location with this script.
+# Name of downloaded backup is $TIMESTAMP(yyyymmdd-hhmmss) + _$BACKUP_FILENAME + export?format(xlsx or csv)
+# resulting file '20240730-013418_Gotham-City-FC-team-uniforms.xlsx'
+# Script logs downloads to "backup_log.txt" file.
 
-# Name of downloaded file is in form $TIMESTAMP + _$BACKUP_FILENAME + .xlsx
-# resulting for example file '20240718-152913_Gotham-City-FC-team-uniforms.xlsx'
+# You can save space with csv formatting. In our case csv file size is 2.0K vs. 91K with xlsx format.
+# If you want to save space using csv files, accordingly replace all string "xlsx" occurrences to
+#  "csv" everywhere in the code including .env file. Be aware that using "sed" tool for this task in
+# following manner "sed -i 's/xlsx/csv/g' google-docs-backup.sh" can also mess up instructions on commented lines.
+# Using csv file format using comes with restrictions. You cannot use advanced features like
+# formulas, formatting, charts, and graphics with csv files. Csv is a plain-text file format used to store tabular data.
+# https://en.wikipedia.org/wiki/Comma-separated_values
 
-# TODO: Space saver
-# Check if contents of last saved file and newly downloaded file is same.
-# If not, keep the newly downloaded file. Else remove newly downloaded file.
-# I got this feature already working using diff with .csv file format downloads.
-
-# Further exploring:
 # To redirect both stdout and stderr to a log file between braces () uncomment
 # two lines below and closing brace in the end of this file.
 
@@ -34,12 +39,10 @@ echo "This is a script for retrieving copy of a shared Google Docs spreadsheet."
 
 # Check if .env file exists
 echo "Checking if .env file exists."
-
 if [ ! -f ".env" ]; then
   echo "Error: .env file not found."
   exit 1
 fi
-
 echo ".env file found."
 sleep 1
 
@@ -48,10 +51,31 @@ set -a
 . .env
 echo "Environment variables loaded."
 
+# Function: current timestamp
+get_timestamp() {
+  TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+}
+
+# Function: log a backup entry
+log_backup() {
+  echo "Backup completed: ${TIMESTAMP}_${BACKUP_FILENAME}.xlsx" >> "${LOG_FILE}"
+}
+
+
+# Check if any csv files with content exist and download one if not
+echo "Checking if any spreadsheets exist."
+if [ $(find . -type f -name "*.xlsx" | wc -l) -eq 0 ]; then
+  get_timestamp
+  curl -s -X GET --location \
+  ${SPREADSHEET_LINK} > "${BACKUP_DIR}/${TIMESTAMP}_${BACKUP_FILENAME}.xlsx"
+echo "1st file '${TIMESTAMP}_${BACKUP_FILENAME}.xlsx' download successful."
+log_backup
+fi
+
 # Function: visual timer
 timer(){
-INTERVAL=3600  # Change this to the desired interval in seconds. 1 hour = 3600 seconds.
-echo "Waiting $INTERVAL seconds until next download."
+INTERVAL=10  # Change this to the desired interval in seconds
+echo "Waiting $INTERVAL seconds until next check of file contents."
 
 for ((i=$INTERVAL; i>=0; i--)); do
     echo -ne "\r$i seconds remaining..."
@@ -64,14 +88,9 @@ timer
 
 LAST_RUN=$(date +%s)
 
-# Function: current timestamp
-get_timestamp() {
-  TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-}
-
-# Function: log a backup entry
-log_backup() {
-  echo "Downloaded: ${TIMESTAMP}_${BACKUP_FILENAME}.xlsx" >> "${LOG_FILE}"
+# Function: get last file
+get_last_file() {
+LAST_FILE=$(find . -type f -name "*.xlsx" | sort -r | head -1)
 }
 
 while true; do
@@ -81,21 +100,36 @@ while true; do
 # Get the current timestamp
 get_timestamp
 
-# Download the spreadsheet as an XLSX file using curl
-  echo "Downloading file..."
-  curl -s -X GET --location \
-    ${SPREADSHEET_LINK} > "${BACKUP_DIR}/${TIMESTAMP}_${BACKUP_FILENAME}.xlsx"
-  echo "Downloaded '${TIMESTAMP}_${BACKUP_FILENAME}.xlsx'"
+# Check and print latest downloaded file
+get_last_file
+    echo "Latest downloaded file: '$LAST_FILE'"
 
+# Download the spreadsheet file using curl
+    echo "Downloading file."
+    curl -s -X GET --location \
+      ${SPREADSHEET_LINK} > "${BACKUP_DIR}/${TIMESTAMP}_${BACKUP_FILENAME}.xlsx"
+    echo "File '${TIMESTAMP}_${BACKUP_FILENAME}.xlsx' download successful."
 
-    # Log the backup entry in the log file
-  log_backup
-  echo "Entry created in '${LOG_FILE}'."
+### FILE COMPARING AND SPACE SAVING SECTION WORKS ONLY WITH CSV TYPE FILE DOWNLOADS ###
+
+    # If content of two last downloaded files are the same, delete the newly downloaded file
+    if diff "${BACKUP_DIR}/${TIMESTAMP}_${BACKUP_FILENAME}.xlsx" "$LAST_FILE" > /dev/null; then
+      echo "Files are same. No new files saved."
+      rm -f "${BACKUP_DIR}/${TIMESTAMP}_${BACKUP_FILENAME}.xlsx"
+ timer
+    # If different, keep new downloaded file.
+    else
+      echo "Files are different. New file saved."
+      log_backup
+      echo "Entry created in '${LOG_FILE}'."
 timer
+    fi
+### FILE COMPARING AND SPACE SAVING SECTION WORKS ONLY WITH CSV TYPE FILE DOWNLOADS ###
 
 LAST_RUN=$CURRENT_TIME
   else
     sleep 1
   fi
 done
+
 #) # this is closing brace for logging stdout and stderr to file
