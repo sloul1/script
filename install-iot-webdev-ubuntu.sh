348e03d22d24fe753386952fd73b70e2dc540adf
Petri Heikkinen <84968030+sloul1@users.noreply.github.com>
2025-01-09 11:34:40 +0200

    add sudo and docker post installation
    add sudos to script and docker post installation instructions

--- install-iot-webdev-ubuntu.sh
+++ install-iot-webdev-ubuntu.sh
@@ -2,17 +2,31 @@
 
 # Created 01/2025 
 
-# This script is created to automate the setup of development environment install on Ubuntu.
+# This script is created to automate the setup of development environment install on Ubuntu 
+# according to the official documentation of: Git, Docker and NodeJS.
 
-# Tested on native Ubuntu 24.04.1 LTS (Noble Numbat) OS,
-# on Ubuntu 24.04.1 LTS (Noble Numbat) docker container
+# https://git-scm.com/
+# https://docs.docker.com/engine/install/
+# https://docs.docker.com/engine/install/ubuntu/
+# https://nodejs.org/en/download
+
+# Tested natively on Ubuntu 24.04.1 LTS (Noble Numbat) OS,
+# on Ubuntu 24.04.1 LTS (Noble Numbat) Docker container
 # and in Windows 11 using WSL and Ubuntu 24.04.1 LTS.
 
 # Script installs: curl, tzdata, git, docker engine community edition, nvm, node  
 
 # Running script on Ubuntu 24.04.1 LTS (Noble Numbat) in docker container
 
-# sudo docker run -ti --rm ubuntu /bin/bash
+# Pre-requisites: post installation steps to run docker without 'sudo' in front of 'docker' command
+# https://docs.docker.com/engine/install/linux-postinstall/
+
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+# docker run hello-world
+
+# docker run -ti --rm ubuntu /bin/bash
 # apt update && apt install curl -y
 # curl https://raw.githubusercontent.com/sloul1/script/refs/heads/main/install-iot-webdev-ubuntu.sh | bash -x
 
@@ -23,33 +37,26 @@
 # sudo apt install curl -y
 # curl -O https://raw.githubusercontent.com/sloul1/script/refs/heads/main/install-iot-webdev-ubuntu.sh
 # chmod +x install-iot-webdev-ubuntu.sh
-# sudo ./install-iot-webdev-ubuntu.sh
-
-# Links to developer tools that will be installed
-
-# https://git-scm.com/
-# https://docs.docker.com/engine/install/
-# https://docs.docker.com/engine/install/ubuntu/
-# https://nodejs.org/en/download
+# ./install-iot-webdev-ubuntu.sh
 
 # Install curl
-apt update && apt install curl -y
+sudo apt update && apt install curl -y
 
 # Create symbolic linka and install 'tzdata' package 
 ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime && apt-get install tzdata -y
 
 # Install git PPA for latest upstream Git version
-add-apt-repository ppa:git-core/ppa -y
-apt update
-apt install git -y
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt update
+sudo apt install git -y
 
 # Install docker engine community edition
 # Remove conflicting packages
-apt-get remove -y docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc
+sudo apt-get remove -y docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc
 
 # Add Docker's offical GPG key:
-apt-get update
-apt-get install ca-certificates curl
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -59,10 +66,10 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
- apt-get update
+sudo apt-get update
 
 # Install
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # verify installation by running hello-world
 # docker run hello-world
