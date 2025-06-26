7aa3caa273d7486b1bc3f8ff47d3c03f58b61414
sloul1 <pethei@proton.me>
2025-06-26 13:34:39 +0300

    add sudo

--- updates.sh
+++ updates.sh
@@ -3,10 +3,10 @@
 # Simple (and crude) script for updating Ubuntu workstation 22.04 software 
 
 # update repositories and upgrade installed software
-apt update && apt upgrade -y
+sudo apt update && apt upgrade -y
 
 # kill snap-store for refreshing
-killall snap-store
+#sudo killall snap-store
 
 # update software installed through snap
-snap refresh
+sudo snap refresh
