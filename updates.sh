80b32228e760f60318f638f399fc995e55fc1f9e
sloul1 <pethei@proton.me>
2025-07-06 12:03:43 +0300

    add package management systems

--- updates.sh
+++ updates.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Simple (and crude) script for updating Ubuntu workstation 22.04 software 
+# Simple script for updating software using different package management
+# systems. Uncomment to fit your purpose.
 
-# update repositories and upgrade installed software
-sudo apt update && apt upgrade -y
+# update aptitude repositories and upgrade installed software
+sudo apt update && sudo apt upgrade -y
 
 # kill snap-store for refreshing
 #sudo killall snap-store
 
 # update software installed through snap
-sudo snap refresh
+#sudo snap refresh
+
+# update software installed through flatpak
+#sudo flatpak update
