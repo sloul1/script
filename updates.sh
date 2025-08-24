d2e7efc183611da9340574d2e6eabfad9ec9f12c
Petri Heikkinen <84968030+sloul1@users.noreply.github.com>
2025-08-24 23:08:24 +0300

    Update updates.sh
    fix 'upgrade' with 'sudo'

--- updates.sh
+++ updates.sh
@@ -3,7 +3,7 @@
 # Simple (and crude) script for updating Ubuntu workstation 22.04 software 
 
 # update repositories and upgrade installed software
-sudo apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 # kill snap-store for refreshing
 #sudo killall snap-store
