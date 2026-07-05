b51f1e08af8065c8bbf9d798ea24de702daf0f5c
Petri Heikkinen <84968030+sloul1@users.noreply.github.com>
2026-07-05 08:06:41 +0300

    Update comments to include restart instructions
    Add instructions for applying changes after modifying configuration files.

--- check_HandleLidSwitch_parameters.sh
+++ check_HandleLidSwitch_parameters.sh
@@ -2,12 +2,16 @@
 
 # This script checks recursively if configuration files (*.conf) in
 # /etc/systemd contain lines including the string "HandleLidSwitch"
+# After modifying for example /etc/systemd/logind.conf run 
+# systemctl restart systemd-logind to apply the changes. 
 
 # Created: 2026/03/19
+# Modified: 2026/07/05
 
 # Tested on:
 #   Fedora Linux 43 (Workstation Edition)
 #   24.04.4 LTS (Noble Numbat)
+#   Proxmox 9.2.4 - Debian Trixie (13.5)
 
 # Set the keyword to search for
 keyword=${1:-"HandleLidSwitch"}
