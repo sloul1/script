d240c98205d1c66fce51b63d4c1158c9c6668629
Petri Heikkinen <84968030+sloul1@users.noreply.github.com>
2026-09-04 10:04:40 +0300

    Enhance toggle_darkmode.ps1 with documentation
    Added comments for clarity and usage instructions.

--- windows/toggle_darkmode.ps1
+++ windows/toggle_darkmode.ps1
@@ -1,3 +1,21 @@
+# Created in 2026/09
+#
+# This script can be used to change Windows registry values to
+# enable darkmode even if the Windows installation isn't activated.
+#
+# Tested on: 
+# 
+# PS> Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber
+#
+# Caption                  Version    BuildNumber
+# -------                  -------    -----------
+# Microsoft Windows 11 Pro 10.0.26200 26200
+#
+# To allow running scripts in Powershell run the following command:
+# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+
+# Then run the script: .\toggle_darkmode.ps1
+
 $ThemePath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
 
 # Set Dark Mode (0)
